77b32f5f86dd3db3a74d59c787e1a63d0391af36
Victor Tolbert <victor.tolbert@gmail.com>
2025-02-26 17:27:06 -0500

    chore: reset to Laravel 12

--- Victor/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Victor.Controllers {
-    public class HomeController : Controller {
-        public IActionResult Index() {
-            return View();
-        }
-    }
-}
